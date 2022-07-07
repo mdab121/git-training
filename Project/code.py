9852a9729d8d0e21a070e01270674fbfff7e3442
Michał Dąbrowski <michal.dabrowski@daftmobile.com>
2022-07-07 15:02:22 +0200

    Add some fancy computation

--- Project/code.py
+++ Project/code.py
@@ -1,9 +1,12 @@
 import os
 
+def squared(n: int) -> int:
+    return n * n
+
 def print_multiple_times(text: str):
     print(text)
     print(text)
 
 def main():
-    print_multiple_times("Hello there!!!")
+    print_multiple_times(f"Hello there {squared(10)} !!!")
     print("Hello world from Daftcode!")

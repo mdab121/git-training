bc293d374a90139b4ab0ed82501eaaaed2a140a3
Michał Dąbrowski <michal.dabrowski@daftmobile.com>
2022-07-07 15:10:54 +0200

    Add exclamation marks

--- Project/code.py
+++ Project/code.py
@@ -5,5 +5,5 @@ def print_multiple_times(text: str):
     print(text)
 
 def main():
-    print_multiple_times("Hello there")
+    print_multiple_times("Hello there!!!")
     print("Hello world from Daftcode!")
